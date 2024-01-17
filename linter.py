2bad0f67b4823ce7d71c9988110bc554833fd4f0
CoGn151oN <121181789+CoGn151oN@users.noreply.github.com>
2024-01-17 01:52:00 +0100

    ref

--- linter.py
+++ linter.py
@@ -6,7 +6,6 @@ run = lint.Run(["--rcfile=.pylintrc", "main.py"], exit=False)
 score = run.linter.stats.global_note
 
 print(f"Score: {score}")
-
 # ez nem kell ahhoz hogy lefusson a linter, csak ha nem éri el a ponthatárt, így is megjeleníti
 if score < THRESHOLD:
     print(f"Linter failed, Score: {score} < threshold")
